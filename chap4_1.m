283654c9c69e3450960c4f463b60cf875f4b3e63
FreyaZhang814 <39469246+FreyaZhang814@users.noreply.github.com>
2020-02-16 16:31:55 -0500

    ex4.1

--- chap4_1.m
+++ chap4_1.m
@@ -1,8 +1,3 @@
-initialsize = [1,3,5,7,9,11]
-finalsize = initialsize + 1
-newsize = sqrt(initialsize)
-nextsize = initialsize.^2
-x = initialsize.*newsize
-y = initialsize./finalsize
-z = 0: 1: 10
-w = linspace(0,10,5)
+
+v = 1: 4: 13
+v1 = linspace(1,13,4)
