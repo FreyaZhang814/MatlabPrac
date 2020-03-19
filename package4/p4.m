cf4a0fa82a7c0408ff450de966fea5d6c48c587a
FreyaZhang814 <39469246+FreyaZhang814@users.noreply.github.com>
2020-03-18 20:12:54 -0400

    finished 4

--- package4/p4.m
+++ package4/p4.m
@@ -6,3 +6,27 @@ x=cumsum(f);
 x=mod(x,L);
 I=zeros(Np,Nk);
 x=ceil(x);
+for i=1:Nk
+    I(:,i)=hist(x(i,:),Np);
+end
+
+figure
+imshow(I(200:400,4500:4800),[]);
+
+vx=zeros(1,Np);
+for i=1:Np
+    vx(i)=var(x(:,i));
+end
+
+ave_x=mean(vx);
+
+figure
+plot(1:Np,vx);
+
+D=ave_x/(2*Nk);
+
+Diffusion=100;
+magn=0.4;
+expTime=0.002;
+
+numP=sqrt(4*Diffusion*expTime)/magn;
