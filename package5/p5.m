158ce30130c9d7b6b6c6f04121fc371039b4e2c4
FreyaZhang814 <39469246+FreyaZhang814@users.noreply.github.com>
2020-03-21 06:06:06 -0400

    5

--- package5/p5.m
+++ package5/p5.m
@@ -16,3 +16,54 @@ lA=conv2(l,A);
 figure(3);
 colormap([co;co;co]');
 image(lA);
+
+meanx=0;meany=0;ssq=0;
+pr=1/(15^2); 
+for i=1:15
+    for j=1:15
+       ssq=ssq+(i^2+j^2)*pr;meany=meany+(j*pr);meanx=meanx+(i*pr);
+    end
+end
+varLarge=ssq-meanx^2-meany^2;
+
+%%gaussian mask
+gauss = zeros(45,45);
+for i = 1:1:45
+  for j = 1:1:45
+    gauss(i,j) = exp(-((i-23)^2 + (j-23)^2)/37);
+  end
+end
+gauss = gauss/sum(gauss(:));
+meanx=0;meany=0;ssq=0;
+for i=1:45
+  for j=1:45
+ssq=ssq+(i^2+j^2)*gauss(i,j);
+meany=meany+(j*gauss(i,j));meanx=meanx+(i*gauss(i,j));
+5
+  end
+end
+varGauss=ssq-meanx^2-meany^2
+figure(4);
+surf(gauss);
+gaussSmoothedA = conv2(gauss, A);
+figure(5);
+colormap([co;co;co]');
+image(gaussSmoothedA);
+
+noisyA=A.*(1.2*rand(size(A)));
+figure(6);
+colormap([co;co;co]');
+image(noisyA);
+denoiseA = conv2(s, noisyA);
+denoiseB = conv2(l, noisyA);
+denoiseC = conv2(gauss, noisyA);
+figure(7);
+colormap([co;co;co]');
+image(denoiseA);%%this is the best
+figure(8);
+colormap([co;co;co]');
+image(denoiseB);
+figure(9);
+colormap([co;co;co]');
+image(denoiseC);
+
